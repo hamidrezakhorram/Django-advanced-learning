0ef0e710775a771a2de7fa15992f4f19b58f37b8
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-01-31 00:54:12 +0330

    add static and media folder

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'blog',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -128,3 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # user manager config
 
 AUTH_USER_MODEL = 'accounts.User'
+
