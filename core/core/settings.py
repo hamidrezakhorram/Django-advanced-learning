795d2d8eb7869b41879003f4ec86a08f47c56405
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-01-30 13:12:46 +0330

    models created

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
+    # 'blog',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# user manager config
+
+AUTH_USER_MODEL = 'accounts.User'
