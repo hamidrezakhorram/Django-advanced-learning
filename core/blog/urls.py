927f346ed2a2b7006d8872ae137dd6bcbc17c98e
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-01-31 01:37:41 +0330

    class base  view added

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import IndexView
 urlpatterns =[
-    # path('' , views.indexViews , name='fbv')
+    path('cbv', IndexView.as_view() , name='cbc' ),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Post
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name']='hamid'
+        context['post']=Post.objects.all()
+        return context
