ce9ef35944e0e4ad4092232312fc7e5c4fa57693
hamidkhorram <hamid.khorram.2020@gmail.com>
2025-02-11 01:48:33 +0330

    serializer aded

--- core/blog/api/v1/view.py
+++ core/blog/api/v1/view.py
@@ -1,10 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializer import PostSerializers
+from blog.models import Post
+from rest_framework  import status
+from django.shortcuts import get_object_or_404
 @api_view()
 def postList(request):
-    return Response('ok')
+    posts = Post.objects.filter(status=True)
+    serializer =PostSerializers(posts , many =True)
+    return Response(serializer.data)
 
 @api_view()
 def postDetail(request , id):
-    return Response(id)
+    post = get_object_or_404(Post , pk=id)
+    serializer =PostSerializers(post)
+    return Response(serializer.data)
+    
+    # try:
+    #      post = Post.objects.get(pk=id)
+    #      serializer =PostSerializers(post)
+    #      return Response(serializer.data)
+    # except Post.DoesNotExist:
+    #     return Response("post does not excite",status=status.HTTP_404_NOT_FOUND)    
+   
