59ae141ee9a6158a39fbf73b95565fbd6e16e274
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2025-03-15 03:37:59 +0330

    review DRF filters

--- core/blog/api/v1/view.py
+++ core/blog/api/v1/view.py
@@ -7,6 +7,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from .pagination import StandardResultsSetPagination
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated] 
@@ -22,9 +26,14 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class PostViewset(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,IsOwnerOrReadOnly]
     serializer_class = PostSerializers
+    pagination_class = StandardResultsSetPagination
     queryset = Post.objects.filter(status=True)
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category', 'auther']
+    search_fields = ['title','auther__first_name']
+    ordering_fields= ['published_date']
 
     # @action(detail=False)
     # def test_extra():
